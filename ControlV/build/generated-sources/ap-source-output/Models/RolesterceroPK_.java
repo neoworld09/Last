e22717d9232a12365773e60d7f3c93eb7573ed39
juancamiloorozcogomez <juancamilocby@gmail.com>
2020-12-05 21:50:31 -0500

    ADICION INTERFAZ DE SST
    creer l interfaz principal de sst pero queda pendiente crear los campos de logueo en la interfaz del INDEX

--- ControlV/build/generated-sources/ap-source-output/Models/RolesterceroPK_.java
+++ ControlV/build/generated-sources/ap-source-output/Models/RolesterceroPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-29T17:43:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-01T23:30:20")
 @StaticMetamodel(RolesterceroPK.class)
 public class RolesterceroPK_ { 
 
